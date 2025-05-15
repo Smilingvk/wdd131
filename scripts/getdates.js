8fad784f0d668803cbc563ee289b0a705ab9059c
Smilingvk <valeriaguzmanavalos@gmail.com>
2025-05-14 22:08:08 -0500

    doneee

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// Año dinámico:
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+// Fecha de última modificación:
+document.getElementById('lastModified').textContent =
+  `Last Modification: ${document.lastModified}`;
